06bf8da935d5ff04ff83683a47f5adc5afa6795e
opiMonitor <pborowskiopi@gmail.com>
2022-04-26 17:36:13 +0200

    testing groovy3

--- job-dsl/test/webtest.groovy
+++ job-dsl/test/webtest.groovy
@@ -1,5 +1,22 @@
 def website = '10.10.33.5/flask'
 
+import groovy.sql.Sql
+// postgresql://postgres:postgres@postgres:5432/postgres
+def url = 'jdbc:postgresql://postgres:5432/postgres'
+def user = 'postgres'
+def password = 'postgres'
+def driver = '"org.postgresql.Driver'
+def sql = Sql.newInstance(url, user, password, driver)
+
+def first = sql.firstRow('SELECT ip FROM host')
+echo first
+
+
+// use 'sql' instance ...
+
+sql.close()
+
+
 //Optional pre-send script, see further in this article for more info.
 //If removed, make sure to also remove the 'presendScript' variable
 //in the publisher block below.
@@ -7,7 +24,7 @@ def website = '10.10.33.5/flask'
 
 for ( i in 0..9 ) {
     //Job identifier, also used for the directory
-    job('website-monitors-test'+i) {
+    job('test/website-monitors-test'+i) {
 
       //Name of the job in Jenkins
       displayName('Website status of multiple containers test'+i)
