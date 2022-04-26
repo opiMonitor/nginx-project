dd9bc3a2e553a622efac5cfb7b7a8f305d62e1cb
opiMonitor <pborowskiopi@gmail.com>
2022-04-26 16:46:16 +0200

    testing groovy2

--- job-dsl/test/webtest.groovy
+++ job-dsl/test/webtest.groovy
@@ -5,56 +5,59 @@ def website = '10.10.33.5/flask'
 //in the publisher block below.
 //def localPreSendScript = readFileFromWorkspace('<path to script>/pre_send_script.groovy_script')
 
-//Job identifier, also used for the directory
-job('website-monitor') {
+for ( i in 0..9 ) {
+    //Job identifier, also used for the directory
+    job('website-monitors-test'+i) {
 
-  //Name of the job in Jenkins
-  displayName('Website status of flask container')
+      //Name of the job in Jenkins
+      displayName('Website status of multiple containers test'+i)
 
-  triggers {
-      //Run every 30 minutes
-      cron('H/30 * * * * ')
-  }
+      triggers {
+          //Run every 30 minutes
+          cron('H/30 * * * * ')
+      }
 
-  steps {
-    environmentVariables {
-      env('TIMEOUT', 5)
-      env('ATTEMPTS', 5)
-    }
+      steps {
+        environmentVariables {
+          env('TIMEOUT', 5)
+          env('ATTEMPTS', 5)
+        }
 
-    //Run a shell script from the workspace
-    shell(readFileFromWorkspace('job-dsl/test/web30.sh'))
-  }
+        //Run a shell script from the workspace
+        shell(readFileFromWorkspace('job-dsl/test/web30.sh'))
+      }
 
-  logRotator {
-    //Remove logs after two days
-    daysToKeep(2)
-  }
+      logRotator {
+        //Remove logs after two days
+        daysToKeep(2)
+      }
 
-  publishers {
-    extendedEmail {
+      publishers {
+        extendedEmail {
 
-        recipientList('pawel.borowski@opi.org.pl')
-        defaultSubject('Oops')
-        defaultContent('Something broken')
-        contentType('text/html')
+            recipientList('pawel.borowski@opi.org.pl')
+            defaultSubject('Oops')
+            defaultContent('Something broken')
+            contentType('text/html')
 
-        triggers {
-                failure {
-                    subject('DSL Task website offline!')
-                    content('website '+ website + ' is offline!')
-                    sendTo {
-                        recipientList('pawel.borowski@opi.org.pl')
+            triggers {
+                    failure {
+                        subject('DSL Task website offline!')
+                        content('website '+ website + ' is offline!')
+                        sendTo {
+                            recipientList('pawel.borowski@opi.org.pl')
+                        }
+                    }
+                    fixed {
+                        subject('DSL Task website fixed!')
+                        content('website '+ website + ' is fixed!')
+                        sendTo {
+                            recipientList('pawel.borowski@opi.org.pl')
+                        }
                     }
                 }
-                fixed {
-                    subject('DSL Task website fixed!')
-                    content('website '+ website + ' is fixed!')
-                    sendTo {
-                        recipientList('pawel.borowski@opi.org.pl')
-                    }
-                }
-            }
+        }
+      }
     }
-  }
+
 }
