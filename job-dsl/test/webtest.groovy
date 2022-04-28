50c07788cb247fdd96e8a756655b1095c9ab4241
opiMonitor <pborowskiopi@gmail.com>
2022-04-28 23:04:53 +0200

    groovy update

--- job-dsl/test/webtest.groovy
+++ job-dsl/test/webtest.groovy
@@ -1,5 +1,8 @@
 def website = '10.10.33.5/flask'
-println System.getProperty("java.ext.dirs")
+echo Hello
+println(System.getProperty("java.ext.dirs"))
+echo System.getProperty("java.ext.dirs")
+echo Hello2
 //def response = sh(script: 'curl http://http://10.10.33.5/flask/urls', returnStdout: true)
 //echo response
 
